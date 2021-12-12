7392d336234799010f29605e8a2dc211c3e2512f
almighty28 <kevincon0028@gmail.com>
2021-12-12 23:18:49 +0200

    Final

---
+++ lb4/lb4.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>ЛБ4</title>
+<meta charset="utf-8">
+</head>
+<body>
+Таблица:
+<?php
+
+echo '<table border = "1">';
+	echo '<tr style = "background-color:red;">';
+		echo '<td>';
+			echo 'Автор';
+		echo '</td>';
+		echo '<td>';
+			echo 'Название';
+		echo '</td>';
+		echo '<td>';
+			echo 'Страницы';
+		echo '</td>';
+	echo '</tr>';
+	echo '<tr style = "background-color:yellow;">';
+		echo '<td>';
+			echo 'С.Кинг';
+		echo '</td>';
+		echo '<td>';
+			echo 'Жребий';
+		echo '</td>';
+		echo '<td>';
+			echo '477';
+		echo '</td>';
+	echo '</tr>';
+		echo '<tr style = "background-color:yellow;">';
+		echo '<td>';
+			echo 'А. Атеев';
+		echo '</td>';
+		echo '<td>';
+			echo 'Карты Люцифера';
+		echo '</td>';
+		echo '<td>';
+			echo '451';
+		echo '</td>';
+	echo '</tr>';
+		echo '<tr style = "background-color:yellow;">';
+		echo '<td>';
+			echo 'Э. Райс';
+		echo '</td>';
+		echo '<td>';
+			echo 'Интервью с вампиром';
+		echo '</td>';
+		echo '<td>';
+			echo '445';
+		echo '</td>';
+	echo '</tr>';
+		echo '<tr style = "background-color:yellow;">';
+		echo '<td>';
+			echo 'Г. Лавкрафт';
+		echo '</td>';
+		echo '<td>';
+			echo 'Дагон';
+		echo '</td>';
+		echo '<td>';
+			echo '9';
+		echo '</td>';
+	echo '</tr>';
+echo '</table>';
+?>
+
+
+</body>
+</html>
